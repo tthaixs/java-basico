b95482a3c177dc6b674282ef78f4a742dc8256f8
Thais Araujo <araujothais.0105@gmail.com>
2023-09-29 17:03:51 -0300

    conta banco pronta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println(" Olá cliente! Digite seu nome, por favor.");
+            String nomecliente = scanner.next();
+            
+            System.out.println("Sua agência.");
+            String agencia = scanner.next();
+
+            System.out.println("Agora, digite o número da Agência ");
+            int numero = scanner.nextInt();
+            
+            System.out.println("Digite seu saldo.");
+            double saldo = scanner.nextDouble();
+
+            
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Ola" + nomecliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        }
+        
     }
 }
